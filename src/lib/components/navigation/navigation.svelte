6e874c9d95a627b36203f44287157ccf0164d9ca
moolenbeek <emoolenbeek@gmail.com>
2024-09-26 22:18:15 -0400

    update nav

--- src/lib/components/navigation/navigation.svelte
+++ src/lib/components/navigation/navigation.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { Button } from "$lib/components/ui/button";
-	import { Sun, Moon, ShoppingCart, Menu } from "lucide-svelte";
+	import { Sun, Moon, ShoppingCart, Menu, User, LogOut } from "lucide-svelte";
 	import { Sheet, SheetContent, SheetTrigger } from "$lib/components/ui/sheet";
+	import { Avatar, AvatarFallback, AvatarImage } from "$lib/components/ui/avatar";
+	import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "$lib/components/ui/dropdown-menu";
 	import { writable } from 'svelte/store';
 	import { onMount } from "svelte";
 
@@ -35,12 +37,9 @@
 			<div class="flex items-center space-x-4">
 				<div class="hidden sm:flex items-center space-x-4">
 					{#if !user}
-						<a href="/login" class="text-sm font-medium hover:text-primary">Login</a>
-					{:else}
-						<a href="/account" class="text-sm font-medium hover:text-primary">{user.firstName} {user.lastName}</a>
-						<form action="/logout" method="POST">
-							<Button type="submit" class="outline">Logout</Button>
-						</form>
+						<Button variant="secondary" size="sm" href="/login">
+							Login
+						</Button>
 					{/if}
 				</div>
 				<Button variant="ghost" size="icon" on:click={toggleDarkMode}>
@@ -53,6 +52,36 @@
 				<Button variant="ghost" size="icon" on:click={() => window.location.href = '/cart'}>
 					<ShoppingCart class="h-[1.2rem] w-[1.2rem]" />
 				</Button>
+				{#if user}
+					<DropdownMenu>
+						<DropdownMenuTrigger>
+							<Avatar>
+								<AvatarImage src={user.avatarUrl} alt={`${user.firstName} ${user.lastName}`} />
+								<AvatarFallback>{user.firstName[0]}{user.lastName[0]}</AvatarFallback>
+							</Avatar>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent>
+							<div class="px-2 py-1.5 text-sm">
+								<p class="font-semibold">{user.firstName} {user.lastName}</p>
+								<p class="text-muted-foreground">{user.email}</p>
+							</div>
+							<DropdownMenuItem>
+								<a href="/account" class="w-full text-sm flex items-center">
+										<User class="h-4 w-4 mr-2" />
+										Account
+								</a>
+							</DropdownMenuItem>
+							<DropdownMenuItem>
+								<form action="/logout" method="POST" class="w-full">
+									<button type="submit" class="w-full text-left text-sm flex items-center">
+										<LogOut class="h-4 w-4 mr-2" />
+										Logout
+									</button>
+								</form>
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				{/if}
 				<div class="sm:hidden">
 					<Sheet bind:open={$isMenuOpen}>
 						<SheetTrigger asChild>
@@ -63,12 +92,16 @@
 						<SheetContent>
 							<nav class="flex flex-col space-y-4">
 								{#if !user}
-									<a href="/login" class="text-sm font-medium hover:text-primary">Login</a>
+									<Button href="/login">Login</Button>
 								{:else}
 									<a href="/">{user.firstName} {user.lastName}</a>
 									<form action="/logout" method="POST">
 										<Button type="submit" class="outline w-full">Logout</Button>
 									</form>
+									<Avatar>
+										<AvatarImage src={user.avatarUrl} alt={`${user.firstName} ${user.lastName}`} />
+										<AvatarFallback>{user.firstName[0]}{user.lastName[0]}</AvatarFallback>
+									</Avatar>
 								{/if}
 							</nav>
 						</SheetContent>
