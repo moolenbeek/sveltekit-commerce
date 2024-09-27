72197269dfef9b8edc6be877c6ef23954c1c2f3f
moolenbeek <emoolenbeek@gmail.com>
2024-09-27 18:47:18 -0400

    add vercel insight

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import { ModeWatcher } from "mode-watcher";
 	import Navigation from "$lib/components/navigation/navigation.svelte";
 	import type { PageData } from "./$types";
+	import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+	injectSpeedInsights();
 
 	export let data: PageData;
 </script>
